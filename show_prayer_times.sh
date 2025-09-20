2279543336ce0a821352859efc32f229afd60417
marzzuki <mwmma5000@gmail.com>
2025-09-20 08:22:08 +0300

    Remove pkill commands from show_prayer_times.sh

--- show_prayer_times.sh
+++ show_prayer_times.sh
@@ -3,11 +3,6 @@ exec > /home/virusminus/prayerbar/prayerbar_debug.log 2>&1
 
 echo "Script started at $(date)"
 
-# Send a signal to waybar to refresh the prayerbar module
-pkill -SIGRTMIN+9 waybar
-
-echo "Waybar signaled at $(date)"
-
 # Get the prayer times tooltip
 echo "Executing prayerbar command at $(date)"
 tooltip=$(/home/virusminus/prayerbar/target/release/prayerbar --city Cairo --country Egypt --method 5 --ampm | jq -r .tooltip | sed 's/<[^>]*>//g')
@@ -18,9 +13,6 @@ echo "Tooltip content: $tooltip"
 html_tooltip=$(echo "$tooltip" | sed ':a;N;s/\n/<br>/g;ta')
 
 # Display as a single notification with HTML interpretation
-notify-send -u low -t 5000 -h "Prayer Times" "$html_tooltip"
+notify-send -u low -t 5000 -h "string:x-canonical-private-markup" "Prayer Times" "$html_tooltip"
 
 echo "Notification sent at $(date)"
-
-# Send a signal to waybar to refresh the prayerbar module again
-pkill -SIGRTMIN+9 waybar
